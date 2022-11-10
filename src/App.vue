52de60d407273c37534a5bafc39457d1625e7616
Sandrava <lacasandra1@gmail.com>
2022-11-10 19:30:59 +0200

    removed default components

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,7 @@ import FetchData from "./components/FetchData.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
     <FetchData />
   </main>
 </template>
