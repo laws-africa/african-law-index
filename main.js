e773dce07aad631b4e96dab7698652ad848a583a
Sandrava <lacasandra1@gmail.com>
2022-11-16 15:27:05 +0200

    added spinner

--- main.js
+++ main.js
@@ -2,124 +2,149 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 const africanLawIndex = createApp({
   template: `<div class="container">
-  <div class="row table-head">
-    <div class="col-1">Rank</div>
-    <div class="col pointer" @click="sortByColumn('location')">Place</div>
-    <div class="col pointer" @click="sortByColumn('legislation')">Legislation</div>
-    <div class="col pointer" @click="sortByColumn('caseLaw')">Case Law</div>
-    <div class="col pointer" @click="sortByColumn('gazette')">Gazettes</div>
-    <div class="col pointer" @click="sortByColumn('score')">Score</div>
-  </div>
-  <div v-for="(access, access_index) in lawIndex" :key="access_index">
-    <div
-      class="row  pointer"
-      data-toggle="collapse"
-      :href="'#' + access.location"
-      role="button"
-      aria-expanded="false"
-      aria-controls="access.location"
-    >
-      <div class="col-1">{{ access_index + 1 }}</div>
-      <div class="col">{{ access.location }}</div>
-      <div class="col">
-        {{ Math.floor((access.legislation.total * 100) / 70) }}%
+    <div v-if="loading" class="d-flex mt-5 justify-content-center">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
       </div>
-      <div class="col">
-        {{ Math.floor((access.caseLaw.total * 100) / 60) }}%
-      </div>
-      <div class="col">
-        {{ Math.floor((access.gazette.total * 100) / 60) }}%
-      </div>
-      <div class="col">{{ Math.floor(access.score) }}%</div>
     </div>
 
-    <div class="collapse row" :id="access.location">
-      <div class="col-1"></div>
-      <div class="col">
-        <div>
-          <p>{{ access.location }}</p>
-          <div class="p-3">
-            <div class="row table-head">
-              <div class="col">Legislation</div>
-              <div class="col"><a :href="access.legislation.website" target="_blank">Description</a></div>
-              <div class="col">Points</div>
-            </div>
-            <div
-              class="row"
-              v-for="(legislation, legislation_index) in access
-                  .legislation.childrenArray"
-              :key="legislation_index"
-            >
-              <div class="col">
-                {{ legislation.cat }} {{ legislation.criterion }}
-              </div>
-              <div class="col">{{ legislation.comments }}</div>
-              <div class="col">{{ legislation.score }}</div>
-            </div>
-            <div class="row">
-              <div class="col font-weight-bold">TOTAL</div>
-              <div class="col"></div>
-              <div class="col font-weight-bold">{{ access.legislation.total }}</div>
-            </div>
+    <div v-else>
+      <div class="row table-head">
+        <div class="col-1">Rank</div>
+        <div class="col pointer" @click="sortByColumn('location')">Place</div>
+        <div class="col pointer" @click="sortByColumn('legislation')">
+          Legislation
+        </div>
+        <div class="col pointer" @click="sortByColumn('caseLaw')">Case Law</div>
+        <div class="col pointer" @click="sortByColumn('gazette')">Gazettes</div>
+        <div class="col pointer" @click="sortByColumn('score')">Score</div>
+      </div>
+      <div v-for="(access, access_index) in lawIndex" :key="access_index">
+        <div
+          class="row pointer"
+          data-bs-toggle="collapse"
+          :data-bs-target="'#' + access.location"
+          role="button"
+          aria-expanded="false"
+          :aria-controls="access.location"
+        >
+          <div class="col-1">{{ access_index + 1 }}</div>
+          <div class="col">{{ access.location }}</div>
+          <div class="col">
+            {{ Math.floor((access.legislation.total * 100) / 70) }}%
           </div>
-
-          <div class="p-3">
-            <div class="row table-head">
-              <div class="col">Case Law</div>
-              <div class="col"><a :href="access.caseLaw.website" target="_blank">Description</a></div>
-              <div class="col">Points</div>
-            </div>
-            <div
-              class="row"
-              v-for="(caseLaw, caseLaw_index) in access
-                  .caseLaw.childrenArray"
-              :key="caseLaw_index"
-            >
-              <div class="col">
-                {{ caseLaw.cat }} {{ caseLaw.criterion }}
-              </div>
-              <div class="col">{{ caseLaw.comments }}</div>
-              <div class="col">{{ caseLaw.score }}</div>
-            </div>
-            <div class="row">
-              <div class="col font-weight-bold">TOTAL</div>
-              <div class="col"></div>
-              <div class="col font-weight-bold">{{ access.caseLaw.total }}</div>
-            </div>
+          <div class="col">
+            {{ Math.floor((access.caseLaw.total * 100) / 60) }}%
           </div>
+          <div class="col">
+            {{ Math.floor((access.gazette.total * 100) / 60) }}%
+          </div>
+          <div class="col">{{ Math.floor(access.score) }}%</div>
+        </div>
 
-          <div class="p-3">
-            <div class="row table-head">
-              <div class="col">Gazettes</div>
-              <div class="col"><a :href="access.gazette.website" target="_blank">Description</a></div>
-              <div class="col">Points</div>
-            </div>
-            <div
-              class="row"
-              v-for="(gazette, gazette_index) in access
-                  .gazette.childrenArray"
-              :key="gazette_index"
-            >
-              <div class="col">
-                {{ gazette.cat }} {{ gazette.criterion }}
+        <div class="collapse row" :id="access.location">
+          <div class="col-1"></div>
+          <div class="col">
+            <div>
+              <p>{{ access.location }}</p>
+              <div class="p-3">
+                <div class="row table-head">
+                  <div class="col">Legislation</div>
+                  <div class="col">
+                    <a :href="access.legislation.website" target="_blank">
+                      Description
+                    </a>
+                  </div>
+                  <div class="col">Points</div>
+                </div>
+                <div
+                  class="row"
+                  v-for="(legislation, legislation_index) in access
+                    .legislation.childrenArray"
+                  :key="legislation_index"
+                >
+                  <div class="col">
+                    {{ legislation.cat }} {{ legislation.criterion }}
+                  </div>
+                  <div class="col">{{ legislation.comments }}</div>
+                  <div class="col">{{ legislation.score }}</div>
+                </div>
+                <div class="row">
+                  <div class="col fw-bold">TOTAL</div>
+                  <div class="col">{{ access.legislation.comments }}</div>
+                  <div class="col fw-bold">
+                    {{ access.legislation.total }}
+                  </div>
+                </div>
               </div>
-              <div class="col">{{ gazette.comments }}</div>
-              <div class="col">{{ gazette.score }}</div>
-            </div>
-            <div class="row">
-              <div class="col font-weight-bold">TOTAL</div>
-              <div class="col"></div>
-              <div class="col font-weight-bold">{{ access.gazette.total }}</div>
+
+              <div class="p-3">
+                <div class="row table-head">
+                  <div class="col">Case Law</div>
+                  <div class="col">
+                    <a :href="access.caseLaw.website" target="_blank">
+                      Description
+                    </a>
+                  </div>
+                  <div class="col">Points</div>
+                </div>
+                <div
+                  class="row"
+                  v-for="(caseLaw, caseLaw_index) in access
+                    .caseLaw.childrenArray"
+                  :key="caseLaw_index"
+                >
+                  <div class="col">{{ caseLaw.cat }} {{ caseLaw.criterion }}</div>
+                  <div class="col">{{ caseLaw.comments }}</div>
+                  <div class="col">{{ caseLaw.score }}</div>
+                </div>
+                <div class="row">
+                  <div class="col fw-bold">TOTAL</div>
+                  <div class="col">{{ access.caseLaw.comments }}</div>
+                  <div class="col fw-bold">
+                    {{ access.caseLaw.total }}
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-3">
+                <div class="row table-head">
+                  <div class="col">Gazettes</div>
+                  <div class="col">
+                    <a :href="access.gazette.website" target="_blank">
+                      Description
+                    </a>
+                  </div>
+                  <div class="col">Points</div>
+                </div>
+                <div
+                  class="row"
+                  v-for="(gazette, gazette_index) in access
+                    .gazette.childrenArray"
+                  :key="gazette_index"
+                >
+                  <div class="col">{{ gazette.cat }} {{ gazette.criterion }}</div>
+                  <div class="col">{{ gazette.comments }}</div>
+                  <div class="col">{{ gazette.score }}</div>
+                </div>
+                <div class="row">
+                  <div class="col fw-bold">TOTAL</div>
+                  <div class="col">{{ access.gazette.comments }}</div>
+                  <div class="col fw-bold">
+                    {{ access.gazette.total }}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>`,
+  </div>`,
   data() {
     return {
       lawIndex: [],
+      loading: true,
     };
   },
   mounted() {
@@ -135,13 +160,16 @@ const africanLawIndex = createApp({
         let json = this.convertToJson(await response.text());
         const lawData = this.formatLawIndex(json);
         this.lawIndex = lawData;
+        this.loading = false;
       } else {
         console.log("HTTP-Error: " + response.status);
       }
     },
     convertToJson(str) {
-      let data = str.split("\r\n").map((i) => i.split(/,(?=\S)/));
-      let headers = data.shift() || [];
+      let data = str
+        .split("\r\n")
+        .map((i) => i.replace(/\"/g, "").split(/,(?=\S)/));
+      let headers = data.shift();
       let output = data.map((d) => {
         let obj = {};
         headers.map((h, i) => (obj[headers[i]] = d[i]));
@@ -207,7 +235,11 @@ const africanLawIndex = createApp({
       objToReturn.points = parseInt(
         this.findDataPerCountry(arr, parseInt(filterValue), "points")
       );
-      objToReturn.comments = this.findDataPerCountry(arr, parseInt(filterValue), "Comments");
+      objToReturn.comments = this.findDataPerCountry(
+        arr,
+        parseInt(filterValue),
+        "Comments"
+      );
 
       return objToReturn;
     },
